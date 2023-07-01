ce7c3a1c0d58f2ed1f3668a982fcc8e5ba3f6080
이승재 [s.lee] <s.lee@devcat.com>
2023-07-01 10:48:57 +0900

    포매팅

--- Application/V-MainForm.cs
+++ Application/V-MainForm.cs
@@ -5,87 +5,86 @@ using Veldrid;
 
 namespace TestEtoVeldrid
 {
-	public partial class MainForm : Form
-	{
-		VeldridSurface Surface;
+    public partial class MainForm : Form
+    {
+        private VeldridSurface Surface;
+        private VeldridDriver Driver;
 
-		VeldridDriver Driver;
+        private bool _veldridReady = false;
+        public bool VeldridReady
+        {
+            get { return _veldridReady; }
+            private set
+            {
+                _veldridReady = value;
 
-		private bool _veldridReady = false;
-		public bool VeldridReady
-		{
-			get { return _veldridReady; }
-			private set
-			{
-				_veldridReady = value;
+                SetUpVeldrid();
+            }
+        }
 
-				SetUpVeldrid();
-			}
-		}
+        private bool _formReady = false;
+        public bool FormReady
+        {
+            get { return _formReady; }
+            set
+            {
+                _formReady = value;
 
-		private bool _formReady = false;
-		public bool FormReady
-		{
-			get { return _formReady; }
-			set
-			{
-				_formReady = value;
+                SetUpVeldrid();
+            }
+        }
 
-				SetUpVeldrid();
-			}
-		}
+        public MainForm() : this(VeldridSurface.PreferredBackend)
+        {
+        }
+        public MainForm(GraphicsBackend backend)
+        {
+            InitializeComponent();
 
-		public MainForm() : this(VeldridSurface.PreferredBackend)
-		{
-		}
-		public MainForm(GraphicsBackend backend)
-		{
-			InitializeComponent();
+            Shown += (sender, e) => FormReady = true;
 
-			Shown += (sender, e) => FormReady = true;
+            // A depth buffer isn't strictly necessary for this project, which uses
+            // only 2D vertex coordinates, but it's helpful to create one for the
+            // sake of demonstration.
+            //
+            // The "improved" resource binding model changes how resource slots are
+            // assigned in the Metal backend, allowing it to work like the others,
+            // so the numbers used in calls to CommandList.SetGraphicsResourceSet
+            // will make more sense to developers used to e.g. OpenGL or Direct3D.
+            var options = new GraphicsDeviceOptions(
+                false,
+                PixelFormat.R32_Float,
+                false,
+                ResourceBindingModel.Improved);
 
-			// A depth buffer isn't strictly necessary for this project, which uses
-			// only 2D vertex coordinates, but it's helpful to create one for the
-			// sake of demonstration.
-			//
-			// The "improved" resource binding model changes how resource slots are
-			// assigned in the Metal backend, allowing it to work like the others,
-			// so the numbers used in calls to CommandList.SetGraphicsResourceSet
-			// will make more sense to developers used to e.g. OpenGL or Direct3D.
-			var options = new GraphicsDeviceOptions(
-				false,
-				PixelFormat.R32_Float,
-				false,
-				ResourceBindingModel.Improved);
+            Surface = new VeldridSurface(backend, options);
+            Surface.Size = new Eto.Drawing.Size(200, 200);
+            Surface.VeldridInitialized += (sender, e) => VeldridReady = true;
 
-			Surface = new VeldridSurface(backend, options);
-			Surface.Size = new Eto.Drawing.Size(200, 200);
-			Surface.VeldridInitialized += (sender, e) => VeldridReady = true;
+            Content = Surface;
 
-			Content = Surface;
+            Driver = new VeldridDriver
+            {
+                Surface = Surface
+            };
 
-			Driver = new VeldridDriver
-			{
-				Surface = Surface
-			};
+            // TODO: Make this binding actually work both ways.
+            CmdAnimate.Bind<bool>("Checked", Driver, "Animate");
+            CmdClockwise.Bind<bool>("Checked", Driver, "Clockwise");
+        }
 
-			// TODO: Make this binding actually work both ways.
-			CmdAnimate.Bind<bool>("Checked", Driver, "Animate");
-			CmdClockwise.Bind<bool>("Checked", Driver, "Clockwise");
-		}
+        private void SetUpVeldrid()
+        {
+            if (!(FormReady && VeldridReady))
+            {
+                return;
+            }
 
-		private void SetUpVeldrid()
-		{
-			if (!(FormReady && VeldridReady))
-			{
-				return;
-			}
+            Driver.SetUpVeldrid();
 
-			Driver.SetUpVeldrid();
+            Title = $"Veldrid backend: {Surface.Backend.ToString()}";
 
-			Title = $"Veldrid backend: {Surface.Backend.ToString()}";
-
-			Driver.Clock.Start();
-		}
-	}
+            Driver.Clock.Start();
+        }
+    }
 }
